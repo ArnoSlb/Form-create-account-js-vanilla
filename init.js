e5390ae01e649e25acdfa3a2c20703138c6128f0
Arnaud Blinkl <soulabaille.arnaud@gmail.com>
2021-03-24 18:14:45 +0100

    feat(email check): add email check with regex

--- init.js
+++ init.js
@@ -7,6 +7,8 @@ const checkboxCGU = document.getElementById('CGU');
 const allCheckIcon = document.querySelectorAll('.icon_check');
 const allAlertMessage = document.querySelectorAll('.alert_message');
 
+// const regexEmail = /\S+@\S+\.\S+/;
+const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 inputUser.addEventListener('input', function(e){
     if(e.target.value.length >= 3){
@@ -18,10 +20,26 @@ inputUser.addEventListener('input', function(e){
     }
 })
 
+inputMail.addEventListener('input', function(e){
+    // search va permettre de vérifier que la valeur de l'input 
+    // match avec le schéma décrit dans regexEmail
+    // Si ça match la valeur sera égale à 0 (l'index du premier résultat), sinon -1
+    if(e.target.value.search(regexEmail) === 0){
+        allCheckIcon[1].style.display = "block"
+        allAlertMessage[1].style.display = "none"
+    }
+    else if(e.target.value.search(regexEmail) === -1){
+        allCheckIcon[1].style.display = "none"
+    }
+})
+
 formRegister.addEventListener('submit', function(e){
     console.log(e.target)
     e.preventDefault();
     if(e.target[0].value.length < 3){
         allAlertMessage[0].style.display = "block"
+    }
+    if(e.target[1].value.search(regexEmail) === -1){
+        allAlertMessage[1].style.display = "block"
     }
 })
