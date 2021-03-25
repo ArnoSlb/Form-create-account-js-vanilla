fe0e67af522361e651565bdfeba390838948a29f
Arnaud Blinkl <soulabaille.arnaud@gmail.com>
2021-03-25 17:33:14 +0100

    fix(all form): add check password, check CGU, and set data in sessionstorage if form is OK

--- init.js
+++ init.js
@@ -1,18 +1,19 @@
 const formRegister = document.getElementById('form_register')
-const inputUser = document.getElementById('user');
-const inputMail = document.getElementById('email');
-const inputPassword = document.getElementById('password');
-const inputPasswordCheck = document.getElementById('password_check');
-const checkboxCGU = document.getElementById('CGU');
-const allCheckIcon = document.querySelectorAll('.icon_check');
-const allAlertMessage = document.querySelectorAll('.alert_message');
+const inputUser = document.getElementById('user')
+const inputMail = document.getElementById('email')
+const inputPassword = document.getElementById('password')
+const inputPasswordCheck = document.getElementById('password_check')
+const checkboxCGU = document.getElementById('CGU')
+const allCheckIcon = document.querySelectorAll('.icon_check')
+const allAlertMessage = document.querySelectorAll('.alert_message')
 const strengthSecurity = document.querySelector('.strength_security')
 const strgthS1 = document.querySelector('.strength_security_1')
 const strgthS2 = document.querySelector('.strength_security_2')
 const strgthS3 = document.querySelector('.strength_security_3')
+const CGU = document.getElementById('CGU')
 
 // const regexEmail = /\S+@\S+\.\S+/;
-const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 // Vérification nom d'utilisateur correct
 inputUser.addEventListener('input', function(e){
@@ -40,7 +41,8 @@ inputMail.addEventListener('input', function(e){
 })
 
 // Vérification mot de passe correct
-let valueInp;
+let valueInp
+let valueInpCheck
 // On définit le caractère spécial, tout ce qui n'est pas (^) une lettre minuscule (a-z) une lettre majuscule (A-Z) ou un nombre (0-9)
 const specialCar = /[^a-zA-Z0-9]/
 // On définit l'alphabet donc tous les caractères (a-z) et pas sensible à la casse, maj ou min (i) pour insensitive
@@ -83,9 +85,6 @@ inputPassword.addEventListener('input', function(e){
 
     testAll = objValidation.symbole + objValidation.letter + objValidation.number;
 
-    console.log(objValidation)
-    console.log(testAll)
-
     if(testAll == 3){
         allAlertMessage[2].style.display = "none"
         allCheckIcon[2].style.display = "block"
@@ -96,38 +95,67 @@ inputPassword.addEventListener('input', function(e){
 
     // Force du mot de passe
     if(valueInp.length > 0 && valueInp.length <= 5){
-        console.log('strgth level 1')
         strengthSecurity.style.display = "flex"
         strgthS1.style.display = "block"
         strgthS2.style.display = "none"
         strgthS3.style.display = "none"
     }
     else if(valueInp.length > 5 && valueInp.length <= 9){
-        console.log('strgth level 2')
         strengthSecurity.style.display = "flex"
         strgthS1.style.display = "block"
         strgthS2.style.display = "block"
         strgthS3.style.display = "none"
     }
     else if(valueInp.length > 9){
-        console.log('strgth level 3')
         strengthSecurity.style.display = "flex"
         strgthS1.style.display = "block"
         strgthS2.style.display = "block"
         strgthS3.style.display = "block"
     }
     else {
-        console.log('strgth level 0')
         strengthSecurity.style.display = "none"
         strgthS1.style.display = "none"
         strgthS2.style.display = "none"
         strgthS3.style.display = "none"
     }
+
+    //Ajustement de la vérification du mot de passe si on change le mot de passe
+    if (valueInpCheck === valueInp && valueInpCheck.length > 0){
+        allCheckIcon[3].style.display = "block"
+        allAlertMessage[3].style.display = "none"
+    }
+    else if (valueInpCheck != undefined){
+        if(valueInpCheck.length == 0){
+            allCheckIcon[3].style.display = "none"
+        }
+    }
+    else{
+        allCheckIcon[3].style.display = "none"
+    }
+})
+
+inputPasswordCheck.addEventListener('input', function(e){
+    valueInpCheck = e.target.value
+    if (valueInpCheck === valueInp && valueInpCheck.length > 0){
+        allCheckIcon[3].style.display = "block"
+        allAlertMessage[3].style.display = "none"
+    }
+    else if (valueInpCheck.length == 0){
+        allCheckIcon[3].style.display = "none"
+    }
+    else{
+        allCheckIcon[3].style.display = "none"
+    }
+})
+
+CGU.addEventListener('click', function(e){
+    if(e.target.checked === true){
+        allAlertMessage[4].style.display = "none"
+    }
 })
 
 // Vérification ensemble input avant envoie et si non affichage message pourquoi
 formRegister.addEventListener('submit', function(e){
-    e.preventDefault();
     if(e.target[0].value.length < 3){
         allAlertMessage[0].style.display = "block"
     }
@@ -136,5 +164,30 @@ formRegister.addEventListener('submit', function(e){
     }
     if(testAll < 3){
         allAlertMessage[2].style.display = "block"
+    }
+    if(e.target[2].value !== e.target[3].value){
+        allAlertMessage[3].style.display = "block"
+    }
+    if(e.target[4].checked === false){
+        allAlertMessage[4].style.display = "block"
+    }
+    
+    if(e.target[0].value.length > 2
+        && e.target[1].value.search(regexEmail) !== -1
+        && testAll > 2
+        && e.target[2].value === e.target[3].value
+        && e.target[4].checked === true){
+            console.log('OK pour envoie')
+            const userAccount = {
+                name : e.target[0].value,
+                email : e.target[1].value,
+                password : valueInp
+            }
+            sessionStorage.setItem('userName', userAccount.name)
+            sessionStorage.setItem('userEmail', userAccount.email)
+            sessionStorage.setItem('userPassword', userAccount.password)
+    }
+    else {
+        e.preventDefault();
     }
 })
